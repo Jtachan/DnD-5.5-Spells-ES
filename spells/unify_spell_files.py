bbbb0b051d33096e88f345b1ef2c8bdb0125da0c
Jaime Gonzalez <76098522+Jtachan@users.noreply.github.com>
2025-07-25 09:22:29 +0200

    Script to fix double line jump

--- spells/unify_spell_files.py
+++ spells/unify_spell_files.py
@@ -3,11 +3,13 @@
 All unified spells have a new keyword 'nivel' showing the level of itself.
 """
 
+import re
 import json
 import os
 
 
 def unify_spells():
+    """Preparing the file 'spells.json' to be loaded by the HTML table."""
     all_spells = []
     for idx in range(10):
         with open(f"level_{idx}.json", "r", encoding="utf-8") as fh:
@@ -18,6 +20,18 @@ def unify_spells():
     out_path = os.path.join(os.path.dirname(__file__), "..", "spells.json")
     with open(out_path, "w", encoding="utf-8") as fh:
         json.dump(all_spells, fh, indent=2)
+
+
+def check_double_line_jump():
+    """Checking if each description has a double line jump `<br><br>`"""
+    pattern = r"(?<!<br>)<br>(?!<br>)"
+    for idx in range(10)
+        with open(f"level_{idx}.json", "r", encoding="utf-8") as fh:
+            spells = json.load(fh)
+        for spell in spells:
+            spell["description"] = re.sub(pattern, "<br><br>", spell["description"])
+        with open(f"level_{idx}.json", "w", encoding="utf-8") as fh:
+            json.dump(spells, fh, indent=2)
 
 
 if __name__ == '__main__':
